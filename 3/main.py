d70f26b157a3bd4bfd69f2cdfa3b351bd13ffc6b
Martin Bergman <martin.bergman@dice.se>
2019-12-06 14:08:56 +0100

    flake8 and measured execution time

--- 3/main.py
+++ 3/main.py
@@ -1,15 +1,17 @@
-with open(r'3/input', 'r') as f:
+# import timeit
+
+with open(r'input', 'r') as f:
     INPUT = f.read()
+
 
 def calcPath(WIRE):
     PATH = {}
     X, Y, INDEX = 0, 0, 0
-
     DIRECTIONS = {
-        'U':[0, 1],
-        'D':[0, -1],
-        'R':[1, 0],
-        'L':[-1, 0]
+        'U': [0, 1],
+        'D': [0, -1],
+        'R': [1, 0],
+        'L': [-1, 0]
     }
     for PART in WIRE:
         LENGTH = PART[1:]
@@ -19,28 +21,33 @@ def calcPath(WIRE):
             X += ADDITION[0]
             Y += ADDITION[1]
             INDEX += 1
-            PATH[(X,Y)] = INDEX
-        
+            PATH[(X, Y)] = INDEX
     return PATH
+
 
 def findShortestPath(INPUT):
     WIRES = [LINE.split(',') for LINE in INPUT.split('\n')]
-    
     WIREPATHS = {}
     for INDEX, WIRE in enumerate(WIRES):
         WIREPATHS[INDEX] = calcPath(WIRE)
-    
     INTERSECTIONS = WIREPATHS[0].keys() & WIREPATHS[1].keys()
 
     LEAST_STEPS = []
     CLOSEST = []
     for INTERSECTION in INTERSECTIONS:
         CLOSEST += [(abs(INTERSECTION[0]) + abs(INTERSECTION[1]))]
-        LEAST_STEPS += [abs(WIREPATHS[0][INTERSECTION]) + abs(WIREPATHS[1][INTERSECTION])]
-
+        LEAST_STEPS += [
+            abs(WIREPATHS[0][INTERSECTION]) +
+            abs(WIREPATHS[1][INTERSECTION])
+        ]
     CLOSEST = min(CLOSEST)
     STEPS = min(LEAST_STEPS)
-    
     return (CLOSEST, STEPS)
+
+
+# elapsed_time = timeit.timeit(
+#      'findShortestPath(INPUT)',
+#      'from __main__ import findShortestPath, INPUT', number=500
+#      )/500
 
 print(findShortestPath(INPUT))
